c5bb2ed99752369f7a9959654a713ba44e14b97b
Joshua Knaven <joshua.knaven@gmail.com>
2021-11-05 13:36:53 +0100

    Finished lava movement and imported PostProcessing

--- Assets/Scripts/LavaMovement.cs
+++ Assets/Scripts/LavaMovement.cs
@@ -5,42 +5,83 @@ using UnityEngine;
 public class LavaMovement : MonoBehaviour
 {
     [SerializeField]
+    SpriteRenderer renderer;
+    [SerializeField]
+    Collider2D collider;
+    [SerializeField]
+    Rigidbody2D rigidbody;
+    [SerializeField]
+    [TagSelector]
+    string playerTag;
+
+    [Header("Start movement")]
+    [SerializeField]
     OnTriggerEnter2DEvent startTrigger;
     [SerializeField]
     Transform startPosition;
     [SerializeField]
     float smoothing = 0.1f;
+
+    [Header("Auto movement")]
     [SerializeField]
-    SpriteRenderer renderer;
+    OnTriggerEnter2DEvent autoMovementTrigger;
     [SerializeField]
-    Collider2D collider;
+    float speed = 1f;
 
     bool lavaIsMoving = false;
 
     void OnEnable()
     {
         startTrigger.onTriggerEnter2D += StartTriggerEnter;
+        autoMovementTrigger.onTriggerEnter2D += AutoTriggerEnter;
     }
 
     void OnDisable()
     {
         startTrigger.onTriggerEnter2D -= StartTriggerEnter;
+        autoMovementTrigger.onTriggerEnter2D -= AutoTriggerEnter;
     }
 
     void StartTriggerEnter(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (!collision.CompareTag(playerTag)) return;
+
+        renderer.enabled = true;
+        collider.enabled = true;
+        startTrigger.enabled = false;
+
+        StartCoroutine(LavaToStartPosition());
+    }
+
+    void AutoTriggerEnter(Collider2D collision)
+    {
+        if (!collision.CompareTag(playerTag)) return;
+        lavaIsMoving = true;
+        rigidbody.WakeUp();
+        rigidbody.velocity = Vector2.up * speed;
+    }
+
+    IEnumerator LavaToStartPosition()
+    {
+        while (!lavaIsMoving)
         {
-            lavaIsMoving = true;
-            renderer.enabled = true;
-            collider.enabled = true;
+            transform.position = Vector3.Lerp(transform.position, startPosition.position, smoothing);
+            if (Vector3.Distance(transform.position, startPosition.position) < 0.1f) yield break; // Stop looping
+
+            yield return null; // Wait for next frame
         }
     }
 
-    void Update()
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag(playerTag)) return;
+
+        Debug.Log("Player hit by lava. Kill player");
+    }
+
+    void FixedUpdate()
     {
         if (!lavaIsMoving) return;
 
-        transform.position = Vector3.Lerp(transform.position, startPosition.position, smoothing);
     }
 }
