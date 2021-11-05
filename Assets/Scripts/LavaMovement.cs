ef263e8478ce13b475c300d7284d92196602d295
Joshua Knaven <joshua.knaven@gmail.com>
2021-11-05 14:18:10 +0100

    Fixed not being able to use namespace Universal

--- Assets/Scripts/LavaMovement.cs
+++ Assets/Scripts/LavaMovement.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Experimental.Rendering;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class LavaMovement : MonoBehaviour
 {
