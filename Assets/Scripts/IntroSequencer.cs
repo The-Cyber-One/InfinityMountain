b9b72f12b2dc1015a7f68c9808ecfe7c6128e172
Joshua Knaven <joshua.knaven@gmail.com>
2021-12-04 14:10:52 +0100

    Added ability to skip scenes and a main menu button

--- Assets/Scripts/IntroSequencer.cs
+++ Assets/Scripts/IntroSequencer.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class IntroSequencer : MonoBehaviour
 {
@@ -17,11 +19,38 @@ public class IntroSequencer : MonoBehaviour
     [SerializeField]
     bool useSceneName = false;
     [SerializeField]
+    Slider skippingProgress;
+    [SerializeField]
+    Image background, fill;
+    [SerializeField]
+    TextMeshProUGUI text;
+    [SerializeField]
+    private float skippingTime = 2f, fadeTime = 0.5f;
+    [SerializeField]
     TextCard[] textCards;
+
+    private float touchTimer = 0;
 
     void Start()
     {
         StartCoroutine(ShowIntroText());
+    }
+
+    private void FixedUpdate()
+    {
+        // Update skipping timer
+        if (Input.touchCount > 0 || Input.anyKey) touchTimer += Time.fixedDeltaTime;
+        else touchTimer = 0;
+
+        // Update slider
+        float fadePercentage = touchTimer / fadeTime;
+        background.color = new Color(background.color.r, background.color.g, background.color.b, fadePercentage); ;
+        fill.color = new Color(fill.color.r, fill.color.g, fill.color.b, fadePercentage);
+        text.alpha = fadePercentage;
+        skippingProgress.value = touchTimer / (skippingTime + fadeTime);
+
+
+        if (touchTimer >= (skippingTime + fadeTime)) LoadNextScene();
     }
 
     IEnumerator ShowIntroText()
@@ -34,7 +63,12 @@ public class IntroSequencer : MonoBehaviour
             yield return new WaitForSecondsRealtime(textCard.time);
         }
 
-        if (useSceneName)SceneManager.LoadScene(nextSceneName);
+        LoadNextScene();
+    }
+
+    void LoadNextScene()
+    {
+        if (useSceneName) SceneManager.LoadScene(nextSceneName);
         else SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
